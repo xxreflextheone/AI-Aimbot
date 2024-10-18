3ce826184a518c2cebc7b9a4c2bfcd6d036ba5c5
Ganon <100484118+xxreflextheone@users.noreply.github.com>
2024-10-18 10:24:07 -0400

    Runs as high priority

--- start.bat
+++ start.bat
@@ -1,2 +1,27 @@
-python lunar.py
-pause
+@echo off
+:: Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+:: If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"="
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+:: Run the Python script as high priority
+start "" /b /realtime python "%~dp0lunar.py"
+
+pause >nul
