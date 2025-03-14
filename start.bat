5779faa8d94a3235cbeaf1f6aee797c62e0aa3a9
Ganon <100484118+xxreflextheone@users.noreply.github.com>
2025-03-14 12:49:29 -0400

    Update start.bat

--- start.bat
+++ start.bat
@@ -10,7 +10,7 @@ if '%errorlevel%' NEQ '0' (
 
 :UACPrompt
     echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    set params = %*:"="
+    set params = %*:"=""
     echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
 
     "%temp%\getadmin.vbs"
@@ -20,8 +20,6 @@ if '%errorlevel%' NEQ '0' (
 :gotAdmin
     pushd "%CD%"
     CD /D "%~dp0"
-
-:: Run the Python script as high priority
-start "" /b /realtime python "%~dp0lunar.py"
-
+:: Run
+python lunar.py
 pause >nul
